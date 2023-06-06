53af5cff861211e36bf83cc6b0eceec4367cbbd5
sifath islam <sifathislam790@gmail.com>
2023-06-06 07:39:20 +0600

    Time complexicity module 1 of dsa

---
+++ DSA/Week 1/Module1/test.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+     int N;
+     cin >> N;
+     for(int i=1;i*2<=N;i++){
+        cout << i << " ";
+     }
+    
+
+    return 0;
+}
