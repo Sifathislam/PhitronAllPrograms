4b2436c316e8c682f35b40146b16278a9d070dfe
sifath islam <sifathislam790@gmail.com>
2023-06-07 14:18:01 +0600

    Module 2 works

---
+++ DSA/Week 1/Module2/tempCodeRunnerFile.cpp
@@ -0,0 +1,3 @@
+emnt of the vector " << v.at(0)<< endl;
+    // cout <<" Access the first elemnt of the vector " << v.front()<< endl;
+    // cout <<" Access the last elemnt of the vector " << v[v.size() - 1]<< endl;
---
+++ DSA/Week 1/Module2/vector_capacity.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    //  Find the length or the size and vector 
+    // vector <int> v(5 ,10);
+    // cout << v.size();
+
+    // Find the max of an vector 
+    // cout << v.max_size();
+
+    // Find the capacity of an vector 
+    // cout << v.capacity()<< endl;
+
+    // v.push_back(159);
+    // cout << v.capacity();
+
+    // To clear an vector 
+    // v.clear();
+    // cout << v.size();
+
+    // Empty kina check kora 
+    // int empt = v.empty();
+    // cout << empt;
+
+    // Resize and vector 
+    // v.resize(10, 1001 );
+
+
+    // cout << v.size();
+
+    return 0;
+}
