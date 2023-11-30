39151f288fc6b3d69b08faa1a0e16c7a040167a1
sifath islam <sifathislam790@gmail.com>
2023-11-30 11:56:13 +0600

    make some edit to tot the code

--- Django/Week 1/Module- 5 Django Template Inheritance Small Project/Main_section/Main_section/urls.py
+++ Django/Week 1/Module- 5 Django Template Inheritance Small Project/Main_section/Main_section/urls.py
@@ -20,6 +20,6 @@ from .import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('index_page', include('index_page.urls')),
+    path('', include('index_page.urls')),
     path('', views.home,name='home')
 ]
