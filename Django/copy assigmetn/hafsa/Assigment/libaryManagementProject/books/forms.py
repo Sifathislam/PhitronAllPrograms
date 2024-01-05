7def58080df19405104f3bef21d2c315c8a69034
sifath islam <sifathislam790@gmail.com>
2024-01-05 11:03:07 +0600

    all the works that i have done in last 10 days

--- Django/copy assigmetn/hafsa/Assigment/libaryManagementProject/books/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import review
-
-
-class reviewForm(forms.ModelForm):
-    class Meta: 
-        model = review
-        fields = ['name', 'body']
--- Django/copy assigmetn/hafsa/Assigment/libaryManagementProject/trasactions/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import DepositMoneyView
-# app_name = 'transactions'
-urlpatterns = [
-    path("deposit/", DepositMoneyView.as_view(), name="deposit_money"),
-]
