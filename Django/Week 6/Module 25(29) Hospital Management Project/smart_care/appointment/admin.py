e4df2ff04594925647fe73cc56da83416c954e72
sifath islam <sifathislam790@gmail.com>
2024-01-08 14:37:09 +0600

    Work of the Module 29 Phitron

--- Django/Week 6/Module 25(29) Hospital Management Project/smart_care/appointment/admin.py
+++ Django/Week 6/Module 25(29) Hospital Management Project/smart_care/appointment/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from .models import Appointment
 # Register your models here.
+class AppointmentAdmin(admin.ModelAdmin):
+    list_display = ['doctorname', 'patientname','appointment_types','appointment_status','symptom','time','cancel']
+
+    
+    def doctorname(self, obj):
+        return obj.doctor.user.first_name
+    def patientname(self, obj):
+        return obj.patinet.user.first_name
+
+
+admin.site.register(Appointment, AppointmentAdmin)
+
